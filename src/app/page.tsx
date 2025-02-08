fccdadcb7d71650fd2ce778ff5c799513c23a714
Syed Omer Ahmed Shamsi <142899185+omershamsi911@users.noreply.github.com>
2025-02-08 23:43:39 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { BackgroundBeams } from "@/components/ui/background-beams";
 import LoginFormDemo from "@/app/login/page";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full bg-neutral-950 relative">
-      <div className="relative z-50">
-        <LoginFormDemo/>
-      </div>
-      <div className="absolute inset-0 z-0">
-        <BackgroundBeams />
-      </div>
+    <main>
+      <LoginFormDemo />
     </main>
   );
 }
